661bc8652cf2748d4e34ef8c01e2e372b7bf7768
Code Guru <codeguru42@gmail.com>
2015-06-24 16:59:26 -0600

    Add member fields and stub out methods in `WorldRenderer`

--- core/src/codeguru/canyonbunny/game/WorldRenderer.java
+++ core/src/codeguru/canyonbunny/game/WorldRenderer.java
@@ -1,4 +1,32 @@
 package codeguru.canyonbunny.game;
 
-public class WorldRenderer {
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
+
+public class WorldRenderer implements Disposable {
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private WorldController controller;
+
+    public WorldRenderer(WorldController controller) {
+
+    }
+
+    private void init() {
+
+    }
+
+    public void render() {
+
+    }
+
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
