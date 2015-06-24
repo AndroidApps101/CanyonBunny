4718e4f4950bb1c02ce7e0c87317d709f21b7be2
Code Guru <codeguru42@gmail.com>
2015-06-24 14:30:22 -0600

    Initial libgdx project

---
+++ desktop/src/codeguru/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package codeguru.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import codeguru.canyonbunny.CanyonBunnyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyGame(), config);
+	}
+}
---
+++ html/src/codeguru/canyonbunny/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package codeguru.canyonbunny.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import codeguru.canyonbunny.CanyonBunnyGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CanyonBunnyGame();
+        }
+}
