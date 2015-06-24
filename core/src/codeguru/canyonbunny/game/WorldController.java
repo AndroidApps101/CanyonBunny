f84a7f1665338658637beb146514f7b8f4721142
Code Guru <codeguru42@gmail.com>
2015-06-24 16:56:25 -0600

    Stub out some methods in `WorldController`

--- core/src/codeguru/canyonbunny/game/WorldController.java
+++ core/src/codeguru/canyonbunny/game/WorldController.java
@@ -1,4 +1,15 @@
 package codeguru.canyonbunny.game;
 
 public class WorldController {
+    public WorldController() {
+
+    }
+
+    private void init() {
+
+    }
+
+    public void update(float deltaTime) {
+
+    }
 }
