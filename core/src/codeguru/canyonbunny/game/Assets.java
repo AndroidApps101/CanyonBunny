6d03e3e2210c42b2a49f8ae4b64696e106a7e21e
Code Guru <codeguru42@gmail.com>
2015-06-25 13:13:16 -0600

    Add `Assets` to manage loading and disposing all assets

---
+++ core/src/codeguru/canyonbunny/game/Assets.java
@@ -0,0 +1,42 @@
+package codeguru.canyonbunny.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+
+import codeguru.canyonbunny.util.Constants;
+
+public class Assets implements Disposable, AssetErrorListener {
+    private static final String TAG = Assets.class.getName();
+    public static final Assets instance = new Assets();
+
+    private AssetManager assetManager;
+
+    private Assets() {
+    }
+
+    public void init(AssetManager assetManager) {
+        this.assetManager = assetManager;
+        assetManager.setErrorListener(this);
+        assetManager.load(Constants.TEXTURE_ATLAS, TextureAtlas.class);
+        assetManager.finishLoading();
+
+        Gdx.app.debug(TAG, "# of assets loaded: " + assetManager.getAssetNames().size);
+        for (String a : assetManager.getAssetNames()) {
+            Gdx.app.debug(TAG, "asset: " + a);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        assetManager.dispose();
+    }
+
+    @Override
+    public void error(AssetDescriptor asset, Throwable throwable) {
+        Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", throwable);
+    }
+}
--- core/src/codeguru/canyonbunny/util/Constants.java
+++ core/src/codeguru/canyonbunny/util/Constants.java
@@ -3,4 +3,5 @@ package codeguru.canyonbunny.util;
 public class Constants {
     public static final float VIEWPORT_WIDTH = 5.0f;
     public static final float VIEWPORT_HEIGHT = 5.0f;
+    public static final String TEXTURE_ATLAS = "images/canyonbunny.atlas";
 }
