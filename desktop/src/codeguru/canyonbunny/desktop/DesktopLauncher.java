55ae7b9e8ae75116e935284e788e0ce92b35618b
Code Guru <codeguru42@gmail.com>
2015-06-25 12:18:55 -0600

    Add title and set size of desktop version

--- desktop/src/codeguru/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/codeguru/canyonbunny/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package codeguru.canyonbunny.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 import codeguru.canyonbunny.CanyonBunnyGame;
 
 public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Canyon Bunny";
+        config.width = 800;
+        config.height = 400;
         new LwjglApplication(new CanyonBunnyGame(), config);
     }
 }
