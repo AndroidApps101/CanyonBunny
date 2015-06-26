5723cd806e27d006ef42ce598cbd1827056f699a
Code Guru <codeguru42@gmail.com>
2015-06-26 14:50:04 -0600

    Add level data

--- core/src/codeguru/canyonbunny/util/Constants.java
+++ core/src/codeguru/canyonbunny/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final float VIEWPORT_WIDTH = 5.0f;
     public static final float VIEWPORT_HEIGHT = 5.0f;
     public static final String TEXTURE_ATLAS = "images/canyonbunny.atlas";
+    public static final String LEVEL_01 = "levels/level-01.png";
 }
