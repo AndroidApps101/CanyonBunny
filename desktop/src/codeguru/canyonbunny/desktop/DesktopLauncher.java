73d6ed893c42e7005ae23d0b89fa3d9a489b9566
Code Guru <codeguru42@gmail.com>
2015-06-26 14:23:25 -0600

    Build texture atlas

--- desktop/src/codeguru/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/codeguru/canyonbunny/desktop/DesktopLauncher.java
@@ -7,7 +7,19 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import codeguru.canyonbunny.CanyonBunnyGame;
 
 public class DesktopLauncher {
+    private static final boolean REBUILD_ATLAS = true;
+    private static final boolean DRAW_DEBUG_OUTLINE = true;
+
     public static void main (String[] arg) {
+        if (REBUILD_ATLAS) {
+            TexturePacker.Settings settings = new TexturePacker.Settings();
+            settings.maxWidth = 1024;
+            settings.maxHeight = 1024;
+            settings.debug = DRAW_DEBUG_OUTLINE;
+            TexturePacker.process(settings, "desktop/assets-raw/images", "android/assets/images",
+                    "canyonbunny");
+        }
+
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Canyon Bunny";
         config.width = 800;
