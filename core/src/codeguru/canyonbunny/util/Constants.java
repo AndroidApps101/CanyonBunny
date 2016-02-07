875eee265e769ddd4cc25d6b750ad246005cf418
Code Apprentice <codeguru42@gmail.com>
2016-02-07 13:01:06 -0700

    Add some constants

--- core/src/codeguru/canyonbunny/util/Constants.java
+++ core/src/codeguru/canyonbunny/util/Constants.java
@@ -3,6 +3,9 @@ package codeguru.canyonbunny.util;
 public class Constants {
     public static final float VIEWPORT_WIDTH = 5.0f;
     public static final float VIEWPORT_HEIGHT = 5.0f;
+    public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+    public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
     public static final String TEXTURE_ATLAS = "images/canyonbunny.atlas";
     public static final String LEVEL_01 = "levels/level-01.png";
+    public static final int LIVES_START = 3;
 }
