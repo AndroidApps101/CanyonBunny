557290f37b7f6cf25f05ba151425e5b805c947e4
Code Apprentice <codeguru42@gmail.com>
2016-02-06 21:06:36 -0700

    Implement `Level.init()`

--- core/src/codeguru/canyonbunny/game/Level.java
+++ core/src/codeguru/canyonbunny/game/Level.java
@@ -1,14 +1,19 @@
 package codeguru.canyonbunny.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 
+import codeguru.canyonbunny.game.objects.AbstractGameObject;
 import codeguru.canyonbunny.game.objects.Clouds;
 import codeguru.canyonbunny.game.objects.Mountains;
 import codeguru.canyonbunny.game.objects.Rock;
 import codeguru.canyonbunny.game.objects.WaterOverlay;
 
 public class Level {
+    private static final String TAG = Level.class.getName();
+
     public enum BLOCK_TYPE {
         EMPTY(0, 0, 0),                   // black
         ROCK(0, 255, 0),                  // green
@@ -45,7 +50,78 @@ public class Level {
     }
 
     private void init(String filename) {
+        // objects
+        rocks = new Array<>();
 
+        // load image file that represents the level data
+        Pixmap pixmap = new Pixmap(Gdx.files.internal(filename));
+        // scan pixels from top-left to bottom-right
+        int lastPixel = -1;
+        for (int pixelY = 0; pixelY < pixmap.getHeight(); pixelY++) {
+            for (int pixelX = 0; pixelX < pixmap.getWidth(); pixelX++) {
+                AbstractGameObject obj = null;
+                float offsetHeight = 0.0f;
+                // height grows from bottom to top
+                float baseHeight = pixmap.getHeight() - pixelY;
+                // get color of current pixel as 32-bit RGBA value
+                int currentPixel = pixmap.getPixel(pixelX, pixelY);
+                // find matching color value to identify block type at (x, y)
+                // point and create the corresponding game object if there is
+                // a match
+                // empty space
+                if (BLOCK_TYPE.EMPTY.sameColor(currentPixel)) {
+                    // do nothing
+                }
+                // rock
+                else if (BLOCK_TYPE.ROCK.sameColor(currentPixel)) {
+                    if (lastPixel != currentPixel) {
+                        obj = new Rock();
+                        float heightIncreaseFactor = 0.25f;
+                        offsetHeight = -2.5f;
+                        obj.position.set(pixelX,
+                                baseHeight * obj.dimension.y * heightIncreaseFactor + offsetHeight);
+                        rocks.add((Rock) obj);
+                    } else {
+                        rocks.get(rocks.size - 1).increaseLength(1);
+                    }
+                }
+                // player spawn point
+                else if (BLOCK_TYPE.PLAYER_SPAWNPOINT.sameColor(currentPixel)) {
+
+                }
+                // feather
+                else if (BLOCK_TYPE.ITEM_FEATHER.sameColor(currentPixel)) {
+
+                }
+                // gold coin
+                else if (BLOCK_TYPE.ITEM_GOLD_COIN.sameColor(currentPixel)) {
+
+                }
+                // unknown object/pixel color
+                else {
+                    int r = 0xff & (currentPixel >>> 24); // red color channel
+                    int g = 0xff & (currentPixel >>> 16); // green color channel
+                    int b = 0xff & (currentPixel >>> 8);  // blue color channel
+                    int a = 0xff & currentPixel;          // alpha channel
+                    Gdx.app.error(TAG, "Unknown object at x<" + pixelX + "> y<" + pixelY
+                            + ">: r>" + r + "> g<" + g + "> b<" + b + "> a<" + a + ">");
+                }
+
+                lastPixel = currentPixel;
+            }
+        }
+
+        // decoration
+        clouds = new Clouds(pixmap.getWidth());
+        clouds.position.set(0.0f, 2.0f);
+        mountains = new Mountains(pixmap.getWidth());
+        mountains.position.set(-1.0f, -1.0f);
+        waterOverlay = new WaterOverlay(pixmap.getWidth());
+        waterOverlay.position.set(0.0f, -3.75f);
+
+        // free memory
+        pixmap.dispose();
+        Gdx.app.debug(TAG, "level '" + filename + "' loaded");
     }
 
     public void render(SpriteBatch batch) {
